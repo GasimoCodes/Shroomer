07c4caef3c14110abad25512dee68811e6a6bcae
Gasimo <prokop.svacina@gmail.com>
2024-03-09 23:40:12 +0100

    Added Shroomie Name

--- Scripts/Map/Shrooms/ShroomGen.cs
+++ Scripts/Map/Shrooms/ShroomGen.cs
@@ -9,6 +9,8 @@ public partial class ShroomGen : ShroomBase
 
 	public ShroomGen()
 	{
+		WaterCost = 5;
+		ShroomName = "MeowShroom :3";
         MaxHealth = 10;
         Health = MaxHealth;
 		icon = GD.Load<Texture2D>("res://Art/Textures/TileSetGround/ground 2.png");

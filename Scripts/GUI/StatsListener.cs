f5836ba6ea46334d51c51ce0138de95cec3290ac
Gasimo <prokop.svacina@gmail.com>
2024-03-09 16:52:56 +0100

    GUI Update

--- Scripts/GUI/StatsListener.cs
+++ Scripts/GUI/StatsListener.cs
@@ -3,13 +3,44 @@ using System;
 
 public partial class StatsListener : Node
 {
+	[Export]
+	public Label energyLabel;
+	[Export]
+	public Label WaterLabel;
+	[Export]
+	public Label HumidityLabel;
+	[Export]
+	public ProgressBar HumidityBar;
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
+		PlayerStats.Instance.Energy.OnValueChanged += (value) =>
+		{
+			energyLabel.Text = "Energy: " + value;
+		};
+
+		PlayerStats.Instance.Water.OnValueChanged += (value) =>
+		{
+			WaterLabel.Text = "Water: " + value;
+		};
+
+		PlayerStats.Instance.Humidity.OnValueChanged += (value) =>
+		{
+			HumidityLabel.Text = "Humidity: " + value;
+			HumidityBar.Value = value;
+		};
+
+
+		// Update vars ez
+		PlayerStats.Instance.Energy.Value = PlayerStats.Instance.Energy.Value;
+		PlayerStats.Instance.Water.Value = PlayerStats.Instance.Water.Value;
+		PlayerStats.Instance.Humidity.Value = PlayerStats.Instance.Humidity.Value;
+		
+
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 }
--- Scripts/PlayerStats.cs
+++ Scripts/PlayerStats.cs
@@ -7,6 +7,7 @@ public partial class PlayerStats : NodeSingleton<PlayerStats>
 
 	public ValueContainer Energy = new ValueContainer();
 	public ValueContainer Water = new ValueContainer();
+	public ValueContainer Humidity = new ValueContainer();
 
 
 
@@ -15,41 +16,53 @@ public partial class PlayerStats : NodeSingleton<PlayerStats>
 	{
 		base._Ready();
 
+
 	}
 
-
-	/// <summary>
-	/// Used to represent stats that can be increased and decreased. 
-	/// </summary>
-	public class ValueContainer
+	public override void _Process(double delta)
 	{
-		private int value = 0;
+		base._Process(delta);
 
-		/// <summary>
-		/// Use this to access the value of the container
-		/// </summary>
-		public int Value
-		{
-			get
-			{
-				return value;
-			}
-			set
-			{
-				if (value > MaxValue)
-				{
-					value = MaxValue;
-				}
-				this.value = value;
-				OnValueChanged?.Invoke(value);
-			}
-		}
-
-		public int MaxValue = 0;
-		public int IncreasePerSecond = 0;
-
-		public Action<int> OnValueChanged;
 
 	}
+
+
+
 
 }
+
+
+/// <summary>
+/// Used to represent stats that can be increased and decreased. 
+/// </summary>
+public class ValueContainer
+{
+	private int value = 0;
+
+	/// <summary>
+	/// Use this to access the value of the container
+	/// </summary>
+	public int Value
+	{
+		get
+		{
+			return value;
+		}
+		set
+		{
+			if (value > MaxValue)
+			{
+				value = MaxValue;
+			}
+			this.value = value;
+			OnValueChanged?.Invoke(value);
+		}
+	}
+
+	public int MaxValue = 0;
+	public int IncreasePerSecond = 0;
+
+	public Action<int> OnValueChanged;
+
+}
+

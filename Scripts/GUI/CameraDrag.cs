b384eb7ca685b5362306213e67466cda0fcc1790
Gasimo <prokop.svacina@gmail.com>
2024-03-09 17:54:05 +0100

    Drag proto

---
+++ Scripts/GUI/CameraDrag.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class CameraDrag : Node2D
+{
+
+	bool btnPressed = false;
+	Vector2 mouseDelta = new Vector2();
+
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			btnPressed = true;
+		}
+		else if (@event is InputEventMouseButton mouseEvent2 && !mouseEvent2.Pressed)
+		{
+			btnPressed = false;
+		}
+		
+		if (@event is InputEventMouseMotion mouseMotion)
+		{
+			if (btnPressed)
+			{
+				mouseDelta = mouseMotion.Relative;
+				Position -= mouseDelta;
+			}
+		}
+	}
+
+
+}
